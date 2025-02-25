300cccbcf5c6b262d6c7276f66d6f7f7c4fb3315
T7280H <amir.bzmara319@outlook.com>
2025-02-25 09:20:20 +0330

    Add files via upload

---
+++ SETUP/systemic/banner/banner.py
@@ -0,0 +1,14 @@
+# banner.py
+
+def print_banner():
+   
+    banner = """
+ ____  ____  ____  ____ 
+||B ||||e ||||t ||||a ||
+||__||||__||||__||||__||
+|/__\||/__\||/__\||/__\|
+    """
+    print(banner)
+
+if __name__ == "__main__":
+    print_banner()
